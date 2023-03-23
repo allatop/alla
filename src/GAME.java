4dcb3b2e3639bd7053221324dc257f89fbef42c1
seva <lasevka1902@mail.ru>
2023-03-23 15:48:30 +0300

    neew

--- src/GAME.java
+++ src/GAME.java
@@ -11,7 +11,7 @@ public class GAME {
             String answ = sc.next();
             if ("да".equals(answ)) {
                 System.out.println("Введите число от 1 до 100");
-                System.out.println(i);
+                //System.out.println(i);
                 int n = sc.nextInt();
                 while (n != i) {
                     if (n < i) {
